2530d7473f4ea31396965d268a805dfa9918cdd5
Mark Smith <hyperoceanic@users.noreply.github.com>
2023-12-02 17:22:48 +0000

    Day 1 part 1

--- Day01/Part01.fs
+++ Day01/Part01.fs
@@ -1,2 +1,16 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System.IO
+open System.Text.RegularExpressions
+
+let regex = Regex(@"\d")
+
+let getScore (str : string)  =
+   let matches = regex.Matches(str)
+   match matches.Count with
+        | 0 -> 0
+        | 1 -> (matches[0].Value |> int) * 11
+        | _ -> (matches[0].Value |> int) * 10 + (matches[matches.Count-1].Value |> int)
+
+let sum =  File.ReadLines(Path.Combine (__SOURCE_DIRECTORY__, "input001.txt" ))
+        |> Seq.fold (fun state (item : string) -> state + getScore item) 0
+
+printfn $"Sum: {sum}"
